9a75de89588fbaa0a0805ada184281a648e44aed
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-01 19:31:06 +0200

    Creating info into OnModelCreating Method

--- BeSocial.Data/BeSocialDbContext.cs
+++ BeSocial.Data/BeSocialDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BeSocial.Data.Models;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BeSocial.Data
@@ -8,6 +10,20 @@ namespace BeSocial.Data
         public BeSocialDbContext(DbContextOptions<BeSocialDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<GroupCategory> GroupCategories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<PostLiker> PostLikers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<PostLiker>()
+                .HasKey(pl => new { pl.LikerId, pl.PostId });
+
+            base.OnModelCreating(builder);
         }
     }
 }
