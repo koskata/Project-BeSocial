4bc3ede0f2e3429bb21374961d031fe841d1fc12
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-01 19:22:11 +0200

    Creating new class - Comment

---
+++ BeSocial.Data.Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSocial.Data.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+    }
+}
