016a5487918c1d701b8e3ef5eff6211e1c03a750
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-12 11:41:39 +0200

    Creating Post Controller and View- All

---
+++ BeSocial.Web/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeSocial.Web.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult All()
+        {
+            //if (User?.Identity?.IsAuthenticated == false || User?.Identity == null)
+            //{
+            //    return RedirectToAction();
+            //}
+            return View();
+        }
+    }
+}
