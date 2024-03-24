b0d521d8b7d98c75e911d98dede09944a4b1775e
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-24 23:32:50 +0200

    Created Post Service and implemented GetAllPosts

---
+++ BeSocial.Services/Interfaces/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BeSocial.Web.ViewModels.Post;
+
+namespace BeSocial.Services.Interfaces
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<PostAllViewModel>> GetAllPostsAsync();
+    }
+}
---
+++ BeSocial.Services/PostService.cs
@@ -0,0 +1,28 @@
+﻿using BeSocial.Data;
+using BeSocial.Services.Interfaces;
+using BeSocial.Web.ViewModels.Post;
+
+using Microsoft.EntityFrameworkCore;
+
+using static BeSocial.Web.Infrastructure.ClaimsPrincipalExtension;
+
+namespace BeSocial.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly BeSocialDbContext context;
+
+        public PostService(BeSocialDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<IEnumerable<PostAllViewModel>> GetAllPostsAsync()
+        {
+            var posts = await context.Posts.Select(x => new PostAllViewModel
+            (x.Title, x.Description, x.Likes, x.CreatedOn, x.PostCategory.Name)).ToListAsync();
+
+            return posts;
+        }
+    }
+}
