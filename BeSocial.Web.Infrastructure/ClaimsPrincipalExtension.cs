5f2f82b4046554d7cb6b7867ffb208cc14e0e1e9
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-24 23:33:32 +0200

    Created Claims Principal Extension

---
+++ BeSocial.Web.Infrastructure/ClaimsPrincipalExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace BeSocial.Web.Infrastructure
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string GetById(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+    }
+}
