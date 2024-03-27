d90b115d470d9ae55ba13c074c38d73d1205bdea
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-27 23:11:27 +0200

    created enum for post sorting

---
+++ BeSocial.Web.ViewModels/Enums/PostSorting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSocial.Web.ViewModels.Enums
+{
+    public enum PostSorting
+    {
+        Newest = 0,
+        MostLikedFirst = 1,
+    }
+}
