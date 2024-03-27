b25e9c4b8aa8a6efcddaa096a89ae0816138561b
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-27 23:12:05 +0200

    Created and implemented models for the searching and pagination

---
+++ BeSocial.Web.ViewModels/Post/AllPostsQueryModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BeSocial.Web.ViewModels.Enums;
+
+namespace BeSocial.Web.ViewModels.Post
+{
+    public class AllPostsQueryModel
+    {
+        public const int PostsPerPage = 3;
+
+        public string Category { get; set; } = null!;
+
+        [Display(Name = "Search by text")]
+        public string SearchTerm { get; set; } = null!;
+
+        public PostSorting Sorting { get; set; }
+
+        [Display(Name = "Current Page")]
+        public int CurrentPage { get; set; } = 1;
+
+        [Display(Name = "Total Posts Count")]
+        public int TotalPostsCount { get; set; }
+
+        public IEnumerable<string> Categories { get; set; } = null!;
+
+        public IEnumerable<PostAllViewModel> Posts { get; set; } = new List<PostAllViewModel>();
+    }
+}
---
+++ BeSocial.Web.ViewModels/Post/PostQueryServiceModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSocial.Web.ViewModels.Post
+{
+    public class PostQueryServiceModel
+    {
+        public int TotalPostsCount { get; set; }
+
+        public IEnumerable<PostAllViewModel> Posts { get; set; } = new List<PostAllViewModel>();
+    }
+}
