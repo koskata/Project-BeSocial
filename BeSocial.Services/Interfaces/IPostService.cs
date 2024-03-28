358437e3c217c491fb4f9c198846877d41db30f5
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-03-28 23:52:15 +0200

    Implemented method for all categories names and made search available

--- BeSocial.Services/Interfaces/IPostService.cs
+++ BeSocial.Services/Interfaces/IPostService.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BeSocial.Web.ViewModels.Enums;
 using BeSocial.Web.ViewModels.Post;
 
 namespace BeSocial.Services.Interfaces
 {
     public interface IPostService
     {
-        Task<IEnumerable<PostAllViewModel>> GetAllPostsAsync();
+        PostQueryServiceModel GetAllPostsAsync(string category = null,
+                                                         string searchTerm = null,
+                                                         PostSorting sorting = PostSorting.Newest,
+                                                         int currentPage = 1,
+                                                         int postsPerPage = 1);
+        Task<IEnumerable<string>> AllPostsCategoriesNames();
     }
 }
--- BeSocial.Services/PostService.cs
+++ BeSocial.Services/PostService.cs
@@ -1,10 +1,13 @@
 ﻿using BeSocial.Data;
 using BeSocial.Services.Interfaces;
+using BeSocial.Web.ViewModels.Enums;
 using BeSocial.Web.ViewModels.Post;
 
 using Microsoft.EntityFrameworkCore;
 
 using static BeSocial.Web.Infrastructure.ClaimsPrincipalExtension;
+using static BeSocial.Common.EntityValidationConstants.Post;
+using System.Linq;
 
 namespace BeSocial.Services
 {
@@ -17,12 +20,60 @@ namespace BeSocial.Services
             context = _context;
         }
 
-        public async Task<IEnumerable<PostAllViewModel>> GetAllPostsAsync()
-        {
-            var posts = await context.Posts.Select(x => new PostAllViewModel
-            (x.Title, x.Description, x.Likes, x.CreatedOn, x.PostCategory.Name)).ToListAsync();
 
-            return posts;
+        public async Task<IEnumerable<string>> AllPostsCategoriesNames()
+        {
+            return await context.PostCategories.Select(c => c.Name).Distinct().ToListAsync();
+        }
+
+
+        public PostQueryServiceModel GetAllPostsAsync(string category = null,
+                                                                            string searchTerm = null,
+                                                                            PostSorting sorting = PostSorting.Newest,
+                                                                            int currentPage = 1,
+                                                                            int postsPerPage = 1)
+        {
+            var postsQuery = context.Posts.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(category))
+            {
+                postsQuery = context.Posts.Where(x => x.PostCategory.Name == category);
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                postsQuery = postsQuery.Where(x =>
+                                                x.Title.ToLower().Contains(searchTerm.ToLower()) ||
+                                                x.Description.ToLower().Contains(searchTerm.ToLower()));
+            }
+
+            postsQuery = sorting switch
+            {
+                PostSorting.MostLiked => postsQuery
+                                                .OrderByDescending(x => x.Likes),
+                _ => postsQuery.OrderByDescending(x => x.CreatedOn)
+            };
+
+            var posts = postsQuery
+                .Skip((currentPage - 1) * postsPerPage)
+                .Take(postsPerPage)
+                .Select(x => new PostAllViewModel(
+                    x.Id,
+                    x.Title,
+                    x.Description,
+                    x.Likes,
+                    x.CreatedOn,
+                    x.PostCategory.Name
+                )).ToList();
+
+
+            var totalPosts = postsQuery.Count();
+
+            return new PostQueryServiceModel()
+            {
+                TotalPostsCount = totalPosts,
+                Posts = posts
+            };
         }
     }
 }
