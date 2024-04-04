70a2925bef6aca3025a7dda1d87c6f5e65dee86f
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-05 00:58:00 +0300

    Created constants for the admin role

---
+++ BeSocial.Common/GeneralApplicationConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSocial.Common
+{
+    public class GeneralApplicationConstants
+    {
+        public const string PremiumUserRoleName = "Premium";
+        public const string PremiumUserAreaName = "Premium";
+
+        public const string PremiumUserDimitarEmail = "dimitarpavlov@gmail.com";
+    }
+}
