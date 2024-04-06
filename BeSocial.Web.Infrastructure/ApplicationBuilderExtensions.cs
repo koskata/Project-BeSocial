e77a7ea70bc2528e7d37dd21b722b7197a64fe13
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-06 13:45:39 +0300

    Created extension for seed new Admin role to DB

---
+++ BeSocial.Web.Infrastructure/ApplicationBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using BeSocial.Data.Models;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+using static BeSocial.Common.GeneralApplicationConstants;
+
+namespace BeSocial.Web.Infrastructure
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task CreateAdminRoleAsync(this IApplicationBuilder app)
+        { 
+            using var scope = app.ApplicationServices.CreateScope();
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+            if (userManager != null && roleManager != null && await roleManager.RoleExistsAsync(AdminRoleName) == false)
+            {
+                var role = new IdentityRole<Guid>(AdminRoleName);
+
+                await roleManager.CreateAsync(role);
+
+                var admin = await userManager.FindByEmailAsync(AdminEmail);
+
+                if (admin != null)
+                {
+                    await userManager.AddToRoleAsync(admin, role.Name);
+                }
+            }
+        }
+    }
+}
