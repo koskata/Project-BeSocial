02745f99e71d2d1597bddd74c59933e09def789b
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-11 00:27:29 +0300

    Created Service for Premium Users

---
+++ BeSocial.Services/Interfaces/IPremiumUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSocial.Services.Interfaces
+{
+    public interface IPremiumUserService
+    {
+        Task<bool> ExistByIdAsync(string userId);
+
+        Task<int> GetPremiumUserId(string userId);
+    }
+}
