7936e485b37d4ccff316cd559462c7bea96d3a9e
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-11 00:25:58 +0300

    Made constants for category error

---
+++ BeSocial.Common/ErrorMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSocial.Common
+{
+    public static class ErrorMessages
+    {
+        public const string RequiredMessage = "The field {0} is required!";
+        public const string LengthErrorMessage = "The field {0} must be between {2} and {1} characters long!";
+
+        //---------
+
+        public const string CategoryDoesNotExist = "Category does not exist.";
+    }
+}
