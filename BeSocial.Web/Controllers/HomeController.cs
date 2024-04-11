6f74afc96a475cebfb61d29d5ccc12ceeabe46b0
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-11 15:08:22 +0300

    Made the connection between modal and the post

--- BeSocial.Web/Controllers/HomeController.cs
+++ BeSocial.Web/Controllers/HomeController.cs
@@ -1,16 +1,42 @@
 ﻿using System.Diagnostics;
 
+using BeSocial.Data;
+using BeSocial.Web.ViewModels.Post;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BeSocial.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BeSocialDbContext context;
 
+        public HomeController(BeSocialDbContext _context)
+        {
+            context = _context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ModalContent(string postId)
+        {
+            //var model = context.Posts.FirstOrDefault(x => x.Id.ToString() == postId);
+
+            var comments = context.Comments
+                .Where(x => x.PostId.ToString() == postId)
+                .Select(x => new PostCommentServiceModel()
+                {
+                    Id = x.Id,
+                    UserFullName = $"{x.User.FirstName} {x.User.LastName}",
+                    Description = x.Description,
+                    PostTitle = x.Post.Title,
+                    PostId = x.PostId.ToString()
+                }).ToList();
+
+            return PartialView("~/Views/Shared/_ModalContent.cshtml", comments);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
