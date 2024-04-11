61e0809414cd094f6ce0e442d2314ac663317870
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-11 15:09:29 +0300

    Removed usings

--- BeSocial.Common/ErrorMessages.cs
+++ BeSocial.Common/ErrorMessages.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeSocial.Common
+﻿namespace BeSocial.Common
 {
     public static class ErrorMessages
     {
--- BeSocial.Services/Interfaces/IPremiumUserService.cs
+++ BeSocial.Services/Interfaces/IPremiumUserService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BeSocial.Services.Interfaces
+﻿namespace BeSocial.Services.Interfaces
 {
     public interface IPremiumUserService
     {
