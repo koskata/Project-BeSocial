5e8ae2e18a158e74c45e75dc0f23d9ef270b8528
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-12 17:57:37 +0300

    Creating Comment controller for the modal

---
+++ BeSocial.Web/Controllers/CommentController.cs
@@ -0,0 +1,37 @@
+﻿using BeSocial.Services.Interfaces;
+using BeSocial.Services.Post;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeSocial.Web.Controllers
+{
+    public class CommentController : Controller
+    {
+
+        private readonly IPostService postService;
+
+        public CommentController(IPostService _postService)
+        {
+            postService = _postService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ModalContent(string postId)
+        {
+            //var model = context.Posts.FirstOrDefault(x => x.Id.ToString() == postId);
+            if (postId == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            var comments = await postService.GetAllCommentsFromPostAsync(postId);
+
+            return PartialView("~/Views/Shared/_ModalContent.cshtml", comments);
+        }
+    }
+}
--- BeSocial.Web/Controllers/HomeController.cs
+++ BeSocial.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using BeSocial.Data;
 using BeSocial.Services.Interfaces;
 using BeSocial.Web.ViewModels.Post;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,29 +12,11 @@ namespace BeSocial.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IPostService postService;
 
-        public HomeController(IPostService _postService)
-        {
-            postService = _postService;
-        }
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
-        }
-
-        public async Task<IActionResult> ModalContent(string postId)
-        {
-            //var model = context.Posts.FirstOrDefault(x => x.Id.ToString() == postId);
-            if (postId == null)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-
-
-            var comments = await postService.GetAllCommentsFromPostAsync(postId);
-
-            return PartialView("~/Views/Shared/_ModalContent.cshtml", comments);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
