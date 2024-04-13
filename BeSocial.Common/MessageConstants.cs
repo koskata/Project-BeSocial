c413e657b30263010b2ba1ca61ff30901dd708af
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-13 22:34:44 +0300

    Added new message constants for the toastr

--- BeSocial.Common/MessageConstants.cs
+++ BeSocial.Common/MessageConstants.cs
@@ -9,5 +9,7 @@ namespace BeSocial.Common
     public static class MessageConstants
     {
         public const string UserMessageSuccess = "UserMessageSuccess";
+        public const string UserMessageWarning = "UserMessageWarning";
+        public const string UserMessageError = "UserMessageError";
     }
 }
