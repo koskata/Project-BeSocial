513a2a20be89131e7d102ad1c624c86361685504
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-13 22:37:13 +0300

    View model for premium user service

---
+++ BeSocial.Web.ViewModels/Premium/PremiumUserAccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSocial.Web.ViewModels.Premium
+{
+    public class PremiumUserAccountViewModel
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+    }
+}
