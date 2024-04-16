00e8690ff9a4bd1a4e80b3b088cb09c121784caf
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-16 23:21:34 +0300

    Create Database mock

---
+++ BeSocial.Tests/Mocks/DatabaseMock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BeSocial.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BeSocial.Tests.Mocks
+{
+    public static class DatabaseMock
+    {
+        public static BeSocialDbContext Instance
+        {
+            get
+            {
+                var dbContextOptions = new DbContextOptionsBuilder<BeSocialDbContext>()
+                    .UseInMemoryDatabase("BeSocialInMemoryDb"
+                        + Guid.NewGuid().ToString()).Options;
+
+                return new BeSocialDbContext(dbContextOptions);
+            }
+        }
+    }
+}
