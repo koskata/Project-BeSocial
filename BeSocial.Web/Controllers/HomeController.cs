e8b08528f410930182066784bcf477bdec7bc0f2
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-04-16 10:06:49 +0300

    Added custom error pages and production environment

--- BeSocial.Web/Controllers/HomeController.cs
+++ BeSocial.Web/Controllers/HomeController.cs
@@ -26,9 +26,19 @@ namespace BeSocial.Web.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return null;
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View();
         }
     }
 }
